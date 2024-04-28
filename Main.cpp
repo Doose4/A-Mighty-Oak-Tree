fcc3fdd79435e9ce83ce7ca85893d3a36e881790
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-04-28 12:33:59 -0500

    Clean up

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-#include "Squirrel.hpp"
 
 using namespace std;
 
+class Squirrel {
+private:
+    string name;
+
+public:
+    Squirrel(string tname) {
+        this->name = tname;
+    }
+
+    string get_name() {
+        return this->name;
+    }
+};
+
 int main() {
+	Squirrel mysquirrel = Squirrel("Jeff");
+	cout << mysquirrel.get_name();
 
 	return 0;
 }
